36bbc13f3efa0e8f474b6bacc47d3b05cad79f8f
ylgr <tu.nguyen@midasprotocol.io>
2021-01-06 15:58:29 +0700

    (+) add log page

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -6,18 +6,21 @@
         <button v-on:click="() => this.getHistory()" :disabled="!extension">Get history</button>
         <button v-on:click="() => this.buyIdo()" :disabled="!extension">Buy Ido</button>
 
+        <p>{{this.log.join(', ')}}</p>
     </div>
 </template>
 
 <script>
     import WalletExtensionUtils from '../utils/walletExtensionUtils';
+    import Vue from 'vue';
     import {extensionName} from '../utils/constants';
 
     export default {
         name: 'Example',
         data: function () {
             return {
-                extension: null
+                extension: null,
+                log: []
             }
         },
         methods: {
@@ -28,19 +31,28 @@
                 this.extension = new WalletExtensionUtils(extension)
             },
             getHistory: async function () {
+                console.log('this.log: ', this.log)
                 console.log('..Test getHistory..')
                 console.log('isConnected: ',this.extension.isConnected())
+                let isConnect = 'isConnected: ' + this.extension.isConnected()
+                Vue.set(this.log, this.log.length, isConnect)
                 const history = await this.extension.getBuyHistoryOfThisAccount()
                 console.log('history: ', history)
+                Vue.set(this.log, this.log.length, 'history: ' + history.map(e => JSON.stringify(e)).join(', '))
+
             },
             buyIdo: async function () {
                 console.log('..Test getHistory..')
                 console.log('isConnected: ',this.extension.isConnected())
-
+                Vue.set(this.log, this.log.length, 'isConnected: ' + this.extension.isConnected())
                 const supportTokenAndBalance = await this.extension.getSupportTokenAndBalance()
                 console.log('getSupportTokenAndBalance: ',supportTokenAndBalance)
+                Vue.set(this.log, this.log.length, 'getSupportTokenAndBalance: ' + supportTokenAndBalance.map(e => JSON.stringify(e)).join(', '))
+
                 const buyResult = await this.extension.buyIdoContractCall(supportTokenAndBalance[0].tokenAddress,1, (msg) => {
                     console.log('buy state: ', msg)
+                    Vue.set(this.log, this.log.length, 'state: ' + msg)
+
                 })
                 console.log('buyResult: ', buyResult)
             }
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/test-web3-vuejs/'
+        : '/'
+}
